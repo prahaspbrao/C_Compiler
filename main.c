aba4465f196348bf01da2e9bdb023f3582c92591
Prahas P B Rao <prahaspbrao2005@gmail.com>
2025-11-22 12:41:43 +0530

    Fully completed code

--- main.c
+++ main.c
@@ -6,6 +6,7 @@
 #include <ctype.h>
 #include <string.h>
 
+
 // First component of the compiler
 // Enumeration for different types of tokens
 typedef enum
@@ -36,7 +37,7 @@ Token *tokenize(const char *input)
         {
             tokens[pos].type = TOKEN_NUMBER;
             // Converting the string to long data
-            tokens[pos].value = strol(input, (char **)&input, 10);
+            tokens[pos].value = strtol(input, (char **)&input, 10);
             pos++;
         }
         else if (*input == '+')
@@ -60,4 +61,105 @@ Token *tokenize(const char *input)
     // Mark the end of the token
     tokens[pos].type = TOKEN_END;
     return tokens;
+}
+
+// Second component of the compiler
+// parser definitions
+// Functions to
+// 1- Parse the tokens
+// 2- Evaluate the expressions
+int parse(Token *tokens, int *result)
+{
+    int pos = 0; // A
+    if (tokens[pos].type != TOKEN_NUMBER)
+    {
+        return 0; // invalid expression
+    }
+    *result = tokens[pos].value;
+    pos++;
+
+    // Loop through the tokens and evaluate the expression
+    while (tokens[pos].type != TOKEN_END)
+    {
+        if (tokens[pos].type == TOKEN_PLUS)
+        {
+            pos++;
+            if (tokens[pos].type != TOKEN_NUMBER)
+            {
+                return 0;
+            }
+
+            *result += tokens[pos].value;
+        }
+        else if (tokens[pos].type == TOKEN_MINUS)
+        {
+            pos++;
+            if (tokens[pos].type != TOKEN_NUMBER)
+            {
+                return 0;
+            }
+            *result -= tokens[pos].value;
+        }
+        else
+        {
+            //Invalid token
+            return 0;
+        }
+        pos++;
+    }
+    return 1; //successfull parsing!!
+}
+
+
+// Third component of compiler
+void generate_assembly(Token *tokens ){
+    int pos = 0;
+    // Load the first number -> register
+    printf("Load %d\n" , tokens[pos].value);
+    pos++;
+
+    while (tokens[pos].type != TOKEN_END) 
+    {
+        if(tokens[pos].type == TOKEN_PLUS){
+            pos++;
+            printf("Add %d\n" , tokens[pos].value);
+        }else if(tokens[pos].type == TOKEN_MINUS){
+            pos++;
+            printf("Sub %d\n" , tokens[pos].value);
+        }
+        pos++;
+    }
+    
+}
+
+// main function
+int main(){
+    // input string
+    char input[256];
+    printf("Enter an arithmetic expression (e.g. 5 + 4 - 3) : ");
+    // Reading the input form the user
+    fgets(input , sizeof(input) , stdin);
+
+    // Removing newline charecter if exists
+    size_t len = strlen(input);
+    // condition
+    if(len > 0 && input[len - 1] == '\n'){
+        input[len - 1] = '\0';
+    }
+
+    // tokenize the input
+    Token *tokens = tokenize(input);
+
+    int result;
+
+    if(parse(tokens , &result)){
+        printf("Parsed result = %d\n" , result);
+        // Generate the assembly-line instructions
+        generate_assembly(tokens);
+    }else {
+        printf("Invalid Expression");
+    }
+    free(tokens);
+    return 0;
+
 }
